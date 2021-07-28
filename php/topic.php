ca34a77657330394e7207754a9d15508a942b5d6
こぴぺたん <takahiro.inoue.0508+github@gmail.com>
2021-07-28 15:54:09 +0900

    トピック生成修正 前日のまとめ生成に30日前の記事を作成、更新する。

--- php/topic.php
+++ php/topic.php
@@ -31,119 +31,112 @@ function hatenaCard($title, $url){
 HTML;
 }
 
-$datetime = date('Y-m-d', strtotime('+9 hour'));
-
-// 日付指定
-if(!is_null($_SERVER['argv'][1] ?? null)){
-    if(date('Y-m-d', strtotime($_SERVER['argv'][1])) != $_SERVER['argv'][1]){
-        echo '日付は Y-m-d 形式で指定してください。' . PHP_EOL;
-        exit;
+function createTopic($datetime){
+    $thumbnails = [
+        0 => 'weekmark7_sun.png',
+        1 => 'weekmark1_mon.png',
+        2 => 'weekmark2_fri.png',
+        3 => 'weekmark3_wed.png',
+        4 => 'weekmark4_thu.png',
+        5 => 'weekmark5_fri.png',
+        6 => 'weekmark6_sat.png',
+    ];
+    
+    // 記事作成日
+    $create = date('Y-m-d\TH:i:s', strtotime($datetime)). '+09:00';
+    // 最終更新日
+    $lastmod = date('Y-m-d\TH:i:s', strtotime(date('Y-m-d', strtotime('+9 hour')))). '+09:00';
+    
+    // 検索対象日時
+    $targetDate = date('Y-m-d', strtotime(date('Y-m-d', strtotime($datetime)) . ' -1 day'));
+    
+    $tags = [
+        // 全般
+        'IT',
+        'プログラミング',
+        'エンジニア',
+        '開発',
+        'SE',
+        'Qiita',
+        'Zenn',
+        'Github',
+    
+        // セキュリティ
+        'セキュリティ',
+        '脆弱性',
+        'XSS',
+        'SQLインジェクション',
+        'CSRF',
+    
+        // 言語
+        'PHP',
+        'Javascript',
+        'Java',
+        'Ruby',
+        'Python',
+        'Flutter',
+        'HTML',
+        'CSS',
+        'SQL',
+    
+        // DB
+        'DB',
+        'Oracle',
+        'Mysql',
+        'Postgres',
+    
+        // OS
+        'Linux',
+        'Windows',
+        'Mac',
+        'Android',
+        'iOS',
+    
+        // クラウド
+        'AWS',
+        'GCP',
+        'Azure',
+    ];
+    
+    $result = [];
+    foreach($tags as $value){
+        $result[$value]= hatena($value, $targetDate, $targetDate);
     }
-    $datetime = $_SERVER['argv'][1];
-}
-
-$thumbnails = [
-    0 => 'weekmark7_sun.png',
-    1 => 'weekmark1_mon.png',
-    2 => 'weekmark2_fri.png',
-    3 => 'weekmark3_wed.png',
-    4 => 'weekmark4_thu.png',
-    5 => 'weekmark5_fri.png',
-    6 => 'weekmark6_sat.png',
-];
-
-// 記事作成日
-$now = date('Y-m-d\TH:i:s', strtotime($datetime)). '+09:00';
-
-// 検索対象日時
-$targetDate = date('Y-m-d', strtotime(date('Y-m-d', strtotime($datetime)) . ' -1 day'));
-
-$tags = [
-    // 全般
-    'IT',
-    'プログラミング',
-    'エンジニア',
-    '開発',
-    'SE',
-    'Qiita',
-    'Zenn',
-    'Github',
-
-    // セキュリティ
-    'セキュリティ',
-    '脆弱性',
-    'XSS',
-    'SQLインジェクション',
-    'CSRF',
-
-    // 言語
-    'PHP',
-    'Javascript',
-    'Java',
-    'Ruby',
-    'Python',
-    'Flutter',
-    'HTML',
-    'CSS',
-    'SQL',
-
-    // DB
-    'DB',
-    'Oracle',
-    'Mysql',
-    'Postgres',
-
-    // OS
-    'Linux',
-    'Windows',
-    'Mac',
-    'Android',
-    'iOS',
-
-    // クラウド
-    'AWS',
-    'GCP',
-    'Azure',
-];
-
-$result = [];
-foreach($tags as $value){
-    $result[$value]= hatena($value, $targetDate, $targetDate);
-}
-
-$tags = '';
-$html = '';
-foreach($result as $ikey => $ivalue){
-    if(count($ivalue) == 0){
-        continue;
-    }
-    $html .= '## ' . $ikey . PHP_EOL;
-    $tags .= '- Topic-' . $ikey . PHP_EOL;
-    foreach($ivalue as $jkey => $jvalue){
-        if($jkey == 3){
-            continue 2;
-        }
-        if(is_array($jvalue['title'])){
+    
+    $tags = '';
+    $html = '';
+    foreach($result as $ikey => $ivalue){
+        if(count($ivalue) == 0){
             continue;
         }
-        if(is_array($jvalue['description'])){
-            $jvalue['description'] = '';
+        $html .= '## ' . $ikey . PHP_EOL;
+        $tags .= '- Topic-' . $ikey . PHP_EOL;
+        foreach($ivalue as $jkey => $jvalue){
+            if($jkey == 5){
+                continue 2;
+            }
+            if(is_array($jvalue['title'])){
+                continue;
+            }
+            if(is_array($jvalue['description'])){
+                $jvalue['description'] = '';
+            }
+    
+            $title = htmlspecialchars(mb_convert_encoding($jvalue['title'], 'utf-8'));
+            $html .= '### ' . $title . PHP_EOL;
+            $html .= '<a href="' . $jvalue['link'] . '" target="_blank" rel="noopener">' . $title . '</a>' . PHP_EOL;
+            $html .= '> ' . htmlspecialchars(mb_convert_encoding($jvalue['description'], 'utf-8')) . PHP_EOL;
+            $html .= hatenaCard($title, $jvalue['link']) . PHP_EOL. PHP_EOL;
         }
-
-        $title = htmlspecialchars(mb_convert_encoding($jvalue['title'], 'utf-8'));
-        $html .= '### ' . $title . PHP_EOL;
-        $html .= '<a href="' . $jvalue['link'] . '" target="_blank" rel="noopener">' . $title . '</a>' . PHP_EOL;
-        $html .= '> ' . htmlspecialchars(mb_convert_encoding($jvalue['description'], 'utf-8')) . PHP_EOL;
-        $html .= hatenaCard($title, $jvalue['link']) . PHP_EOL. PHP_EOL;
     }
-}
 
-$thumbnail = $thumbnails[date('w', strtotime($targetDate))];
+    $thumbnail = $thumbnails[date('w', strtotime($targetDate))];
 
-$data = <<<TOPIC
+    $data = <<<TOPIC
 ---
 title: "{$targetDate} の話題まとめ"
-date: {$now}
+date: {$create}
+lastMod: {$lastmod}
 description: "{$targetDate} の話題まとめ"
 draft: false
 enableToc: true
@@ -163,7 +156,24 @@ image: images/thumbnail/{$thumbnail}
 {$html}
 
 TOPIC;
+    
+    $output = realpath(__DIR__ . '/../blog/content/ja/topic/') . '/' . $targetDate . '.md';
+    file_put_contents($output, $data);
+    echo 'Created Topic ' . $output . PHP_EOL;
+}
 
-$output = realpath(__DIR__ . '/../blog/content/ja/topic/') . '/' . $targetDate . '.md';
-file_put_contents($output, $data);
-echo 'Created Topic ' . $output . PHP_EOL;
+$datetime = date('Y-m-d', strtotime('+9 hour'));
+
+// 日付指定
+if(!is_null($_SERVER['argv'][1] ?? null)){
+    if(date('Y-m-d', strtotime($_SERVER['argv'][1])) != $_SERVER['argv'][1]){
+        echo '日付は Y-m-d 形式で指定してください。' . PHP_EOL;
+        exit;
+    }
+    $datetime = $_SERVER['argv'][1];
+}
+
+// 昨日の話題を作成
+createTopic($datetime);
+// 1ヶ月前の話題を再生成
+createTopic(date('Y-m-d', strtotime($datetime . ' -30day')));

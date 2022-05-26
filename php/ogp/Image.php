e742e281ecdc5a395b482cfed694fb54f937bd5b
こぴぺたん <takahiro.inoue.0508+github@gamail.com>
2022-05-27 07:56:24 +0900

    OGP修正

--- php/ogp/Image.php
+++ php/ogp/Image.php
@@ -38,7 +38,8 @@ class Image
             implode(PHP_EOL, mb_str_split($text, 25))
         );
         // ファイル名を指定して画像出力
-        @mkdir(dirname($output), '0777', TRUE);
+        echo dirname($output) . PHP_EOL;
+        @mkdir(dirname($output), '0777', true);
         imagepng($image, $output);
     }
 }

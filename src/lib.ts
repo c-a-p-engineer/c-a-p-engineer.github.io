1b1d8a8ee33dbd124689b37df82306b95dc8eac2
こぴぺたん <67095323+c-a-p-engineer@users.noreply.github.com>
2026-09-03 19:31:57 +0900

    feat: derive TechBookFest OGP images

--- src/lib.ts
+++ src/lib.ts
@@ -1,2 +1,22 @@
 import { getCollection } from 'astro:content';
-export const articles = async () => (await getCollection('articles', ({ data }) => !data.draft)).sort((a,b) => b.data.date.valueOf() - a.data.date.valueOf());
+
+export const articles = async () =>
+  (await getCollection('articles', ({ data }) => !data.draft)).sort(
+    (a, b) => b.data.date.valueOf() - a.data.date.valueOf(),
+  );
+
+export const techBookFestOgp = (href?: string) => {
+  if (!href) return undefined;
+
+  try {
+    const url = new URL(href);
+    if (url.hostname !== 'techbookfest.org') return undefined;
+
+    const productId = url.pathname.match(/^\/product\/([^/]+)/)?.[1];
+    return productId
+      ? `https://techbookfest.org/api/product/ogp/image/${productId}`
+      : undefined;
+  } catch {
+    return undefined;
+  }
+};
